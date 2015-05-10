cd9d3c0cb49ba766b1208a3fec8332efae09977f
Judd Flamm <kd7yva@gmail.com>
2015-05-09 23:33:05 -0600

    working on code :)

--- oled.js
+++ oled.js
@@ -65,6 +65,7 @@ var Oled = function(opts) {
   };
 
   // Setup i2c
+  console.log('this.ADDRESS: ' + this.ADDRESS);
   this.wire = new i2c(this.ADDRESS, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
   var screenSize = this.WIDTH + 'x' + this.HEIGHT;
